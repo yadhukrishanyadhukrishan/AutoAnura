4c57688b9704395d04927957f014277b63a2ea6a
mallumoviesearch <103921432+mallumoviesearch@users.noreply.github.com>
2022-12-13 20:13:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/AutoAnura /AutoAnura
+  git clone https://github.com/mallumoviesearch/AutoAnura /AutoAnura
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoAnura
